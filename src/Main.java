edc244125d86035868a119bd489927c35a5a92d5
ruslanzverkov <createmed@mail.ru>
2022-07-31 21:40:59 +0300

    DZ1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        Первое задание
+
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника - "+fullName);
+
+//        Вторе задание
+
+
     }
 }
